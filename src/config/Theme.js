a1bd521e01a106c5958adb8d8b3faed4b82bb95a
Raquel Calado <raquel@jetpax.com.br>
2020-03-29 21:44:46 -0300

    Adds a global theme file to the project and exports colors.

---
+++ src/config/Theme.js
@@ -0,0 +1,25 @@
+const Colors = {
+  primary: '#8b10ae',
+  purplelight: '#dda6ed',
+  white: '#FFF',
+  grey: '#999',
+  greylight: '#eee',
+  greydark: '#333',
+  greydarksecundary: '#666'
+};
+
+const NubankTheme = {
+  name: 'NubankTheme',
+  colors: {
+    primary: Colors.primary,
+    white: Colors.white,
+    purplelight: Colors.purplelight,
+    white: Colors.white,
+    grey: Colors.grey,
+    greylight: Colors.greylight,
+    greydark: Colors.greydark,
+    greydarksecundary: Colors.greydarksecundary
+  },
+};
+
+export default NubankTheme;
